721b1dc5dc5a78e26898444a2f3c761b9b814400
RCsoominlee <ssuminmin95@naver.com>
2022-06-02 14:19:51 +0900

    align folder name

---
+++ DB/graph_conversion.py
@@ -0,0 +1,108 @@
+import os
+import pickle
+import glob
+from rdkit import Chem
+from tqdm import tqdm
+
+from libs.preprocess_utils import Graph, select_residue
+from libs.db_utils import connect2pgSQL as pgDB
+
+def convert_dataset(db:pgDB, query, experiment_type='training'):
+
+    result = db.execute(query)
+    root_PATH = 'D:/Data/PDBbind/raw_data'
+
+    pdb_code_list = []
+    mol_ligand_list = []
+    mol_residues_list = []
+
+    with tqdm(total=len(result)) as pbar:
+        pbar.set_description('load molecule data')
+        for _, data in enumerate(result):
+            pbar.update(1)
+            pdb_code = data[0]
+            subset = data[1]
+
+            if subset == 'general':
+                PATH = f'{root_PATH}/PDBbind_v2020/{pdb_code}'
+            elif subset == 'refined':
+                PATH = f'{root_PATH}/refined-set_v2020/{pdb_code}'
+            elif subset == 'coreset':
+                PATH = f'{root_PATH}/CASF-2016/coreset/{pdb_code}'
+
+            lPATH_sdf = f'{PATH}/{pdb_code}_ligand.sdf'
+            lPATH_mol2 = f'{PATH}/{pdb_code}_ligand.mol2'
+            pPATH_pck = f'{PATH}/{pdb_code}_pocket.pdb'
+            pPATH_rsd = f'D:/Data/PDBbind/HD011/model_input/{experiment_type}/residues/{pdb_code}_residues.pdb'
+
+            mol_ligand = Chem.SDMolSupplier(lPATH_sdf)[0]
+            if not mol_ligand:
+                mol_ligand = Chem.MolFromMol2File(lPATH_mol2)
+            try:
+                if os.path.isfile(pPATH_rsd):
+                    mol_residues = Chem.MolFromPDBFile(pPATH_rsd)
+                else:
+                    # D:/Data/PDBbind/preprocessed/HD011-training/residues
+                    mol_residues = select_residue(mol_ligand, pPATH_pck,
+                                                  save_path=f'D:/Data/PDBbind/preprocessed/HD011-{experiment_type}/residues')
+            except:
+                pass
+
+            pdb_code_list.append(pdb_code)
+            mol_ligand_list.append(mol_ligand)
+            mol_residues_list.append(mol_residues)
+
+    with tqdm(total=len(result)) as pbar2:
+        pbar2.set_description('convert to graph')
+
+        exstGrph = os.listdir(f'D:/Data/PDBbind/preprocessed/HD011-{experiment_type}/graph/')
+        exstGrphs = [graph.split('.')[0] for graph in exstGrph if graph.endswith('.pkl')]
+
+        for id, lig, rsd in zip(pdb_code_list, mol_ligand_list, mol_residues_list):
+            pbar2.update(1)
+            if id in exstGrphs:
+                continue
+
+            if (lig is None) | (rsd is None):
+                print(f'\n<!> {id} is NoneType')
+                # updateDB(self, schema, table, column, value, c_column, condition):
+                # sql = " UPDATE {schema}.{table} SET {column}=\'{value}\' WHERE {c_column}=\'{condition}\'"\
+                inav_type = f"SELECT inavailability_type FROM pdbbind.available WHERE pdb_code='{id}'"
+                inav_type = db.execute(inav_type)[0][0]
+                db.commit()
+                if inav_type == 'None':
+                    inav_type = 'empty_rsd_file'
+                else:
+                    inav_type = f"{inav_type}, empty_rsd_file"
+
+                try:
+                    query_up = f"UPDATE pdbbind.available SET available=False, " \
+                               f"inavailability_type = '{inav_type}' WHERE pdb_code='{id}'"
+                    result = db.execute(query_up)
+
+                    db.commit()
+                except:
+                    print(query_up)
+                    continue
+
+                continue
+
+            graph_l = Graph(molecule=lig).generate()
+            graph_p = Graph(molecule=rsd).generate()
+
+            save_path = f'D:/Data/PDBbind/preprocessed/HD011-{experiment_type}/graph'
+
+            with open(f'{save_path}/{id}.pkl', 'wb') as f:
+                pickle.dump((graph_l, graph_p), f, pickle.HIGHEST_PROTOCOL)
+
+
+if __name__ == '__main__':
+    db = pgDB()
+    query = 'SELECT binding.pdb_code, binding.subset FROM pdbbind.binding ' \
+            'INNER JOIN pdbbind.available ON binding.pdb_code = available.pdb_code ' \
+            'WHERE available.available = TRUE AND binding.pdb_code NOT IN (SELECT pdb_code FROM pdbbind.coreset) AND available.pdb_code=\'2r1w\''
+
+    query_coreset = 'SELECT coreset.pdb_code, available.subset FROM pdbbind.coreset ' \
+                    'INNER JOIN pdbbind.available ON coreset.pdb_code = available.pdb_code ' \
+                    'WHERE available.available=TRUE AND available.subset=\'coreset\''
+    convert_dataset(db, query_coreset, experiment_type='ex_test')
---
+++ DB/libs/filtering_utils.py
@@ -0,0 +1,173 @@
+import os
+import time
+import sys
+import pandas as pd
+import numpy as np
+
+from rdkit import Chem
+from Bio.PDB import *
+from tqdm import tqdm
+
+from db_utils import connect2pgSQL as pgDB
+
+def extract_list(db:pgDB, columns:list, schema:str, table:str):
+    columns = ', '.join(columns)
+
+    query = f'SELECT {columns} FROM {schema}.{table};'
+    result = db.execute(query)
+    return result
+
+
+def _no_Folder(org_data_list:pd.DataFrame):
+    result = []
+
+    with tqdm(total=len(org_data_list)) as pbar:
+        pbar.set_description('Check folder ')
+        for i, row in org_data_list.iterrows():
+            pbar.update(1)
+            path_tmp = f"{row['path']}{row['pdb_code']}/"
+            result.append(not os.path.isdir(path_tmp))
+
+    return result
+
+
+def _no_lig_file(org_data_list:pd.DataFrame):
+    result_sdf = []
+    result_mol2 = []
+
+    with tqdm(total=len(org_data_list)) as pbar:
+        pbar.set_description('Check ligand file ')
+        for i, row in org_data_list.iterrows():
+            pbar.update(1)
+            id = row['pdb_code']
+            path_tmp = f"{row['path']}{id}/"
+            if not os.path.isdir(path_tmp):
+                result_sdf.append(True)
+                result_mol2.append(True)
+            else:
+                f_list = os.listdir(path_tmp)
+                result_sdf.append(not f'{id}_ligand.sdf' in f_list)
+                result_mol2.append(not f'{id}_ligand.mol2' in f_list)
+
+    return result_sdf, result_mol2
+
+
+def _no_ptn_file(org_data_list:pd.DataFrame):
+    result_protein = []
+    result_pocket = []
+
+    with tqdm(total=len(org_data_list)) as pbar:
+        pbar.set_description('Check protein file ')
+        for i, row in org_data_list.iterrows():
+            pbar.update(1)
+            id = row['pdb_code']
+            path_tmp = f"{row['path']}{id}/"
+            if not os.path.isdir(path_tmp):
+                result_protein.append(True)
+                result_pocket.append(True)
+            else:
+                f_list = os.listdir(path_tmp)
+                result_protein.append(not f'{id}_protein.pdb' in f_list)
+                result_pocket.append(not f'{id}_pocket.pdb' in f_list)
+
+    return result_protein, result_pocket
+
+
+def _empty_lig(org_data_list:pd.DataFrame):
+    result_sdf = []
+    result_mol2 = []
+
+    with tqdm(total=len(org_data_list)) as pbar:
+        pbar.set_description('Check ligand file contents ')
+        for i, row in org_data_list.iterrows():
+            pbar.update(1)
+            id = row['pdb_code']
+            path_sdf = f"{row['path']}{id}/{id}_ligand.sdf"
+            path_mol2 = f"{row['path']}{id}/{id}_ligand.mol2"
+
+            if not os.path.isfile(path_sdf):
+                result_sdf.append(True)
+            else:
+                if Chem.SDMolSupplier(path_sdf)[0]:
+                    result_sdf.append(False)
+                else:
+                    result_sdf.append(True)
+
+            if not os.path.isfile(path_mol2):
+                result_mol2.append(True)
+            else:
+                if Chem.MolFromMol2File(path_mol2):
+                    result_mol2.append(False)
+                else:
+                    result_mol2.append(True)
+
+    return result_sdf, result_mol2
+
+
+def _empty_ptn(org_data_list:pd.DataFrame):
+    result_protein = []
+    result_pocket = []
+
+    with tqdm(total=len(org_data_list)) as pbar:
+        pbar.set_description('Check protein file contents ')
+        for i, row in org_data_list.iterrows():
+            pbar.update(1)
+            id = row['pdb_code']
+            path_protein = f"{row['path']}{id}/{id}_protein.pdb"
+            path_pocket = f"{row['path']}{id}/{id}_pocket.pdb"
+
+            if not os.path.isfile(path_protein):
+                result_protein.append(True)
+            else:
+                if Chem.MolFromPDBFile(path_protein):
+                    result_protein.append(False)
+                else:
+                    result_protein.append(True)
+
+            if not os.path.isfile(path_pocket):
+                result_pocket.append(True)
+            else:
+                if Chem.MolFromPDBFile(path_pocket):
+                    result_pocket.append(False)
+                else:
+                    result_pocket.append(True)
+
+    return result_protein, result_pocket
+
+
+def filtering(src:pd.DataFrame):
+    # filtering
+    filtered_data = pd.DataFrame()
+    filtered_data['pdb_code'] = src['pdb_code']
+    filtered_data['no_folder'] = _no_Folder(src)
+    filtered_data['no_ptn_file'], filtered_data['no_pck_file'] = _no_ptn_file(src)
+    filtered_data['no_lig_file_sdf'], filtered_data['no_lig_file_mol2'] = _no_lig_file(src)
+    filtered_data['empty_ptn_file'], filtered_data['empty_pck_file'] = _empty_ptn(src)
+    filtered_data['empty_lig_file_sdf'], filtered_data['empty_lig_file_mol2'] = _empty_lig(src)
+
+    filtered_data['available'] = True
+
+    filtered_data.loc[filtered_data['no_folder'] == True, 'available'] = False
+    filtered_data.loc[filtered_data['no_ptn_file'] == True, 'available'] = False
+
+    # true는 fillna 사용
+    filtered_data.loc[(filtered_data['no_lig_file_sdf'] == True) &
+                      (filtered_data['no_lig_file_mol2'] == True), 'available'] = False
+    filtered_data.loc[(filtered_data['empty_lig_file_sdf'] == True) &
+                      (filtered_data['empty_lig_file_mol2'] == True), 'available'] = False
+    filtered_data.loc[(filtered_data['empty_pck_file'] == True) &
+                      (filtered_data['empty_pck_file'] == True), 'available'] = False
+    filtered_data['subset'] = src['subset']
+
+    caller = sys._getframe(1).f_code.co_name
+    f_name = f"./Report/check_inavailability_({caller})-{time.strftime('%Y-%m-%d', time.localtime(time.time()))}.csv"
+    if os.path.isfile(f_name):
+        filtered_data.to_csv(f_name, mode='a', header=False, index=False)
+    else:
+        filtered_data.to_csv(f_name)
+
+    return filtered_data
+
+
+if __name__ == '__main__':
+    pass
